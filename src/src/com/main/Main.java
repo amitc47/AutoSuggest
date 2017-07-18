035b74448254f4d1f1c88285ae5ea9b57027d56b
Amit Choudhary <amitc47@gmail.com>
2017-07-18 08:39:01 +0530

    Amit Choudhary : Add files

---
+++ src/src/com/main/Main.java
@@ -0,0 +1,11 @@
+package src.com.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Good morning!!!");
+	}
+
+}
